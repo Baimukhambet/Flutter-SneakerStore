5b8424c82b161793fc9124f6c11a115af60b8f50
PP2_21B031121 <vendetta10090@gmail.com>
2024-04-17 01:11:39 +0500

    Add Menu buttons, Drawer, Builder

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -28,6 +28,54 @@ class _HomePageState extends State<HomePage> {
       backgroundColor: Colors.grey[300],
       bottomNavigationBar: BottomNavBar(onTabChange: (index) => navigateBottomBar(index),),
       body: _pages[_currentIndex],
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+              onPressed: (){Scaffold.of(context).openDrawer();},
+              icon: const Icon(Icons.menu, color: Colors.black)
+              );
+          }
+        ),
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.grey[900],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+          Column(
+            children: [
+          DrawerHeader(child: Image.asset('lib/images/logo.png',
+          color: Colors.white,
+          width: 120,
+          ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 25.0),
+            child: ListTile(
+              leading: Icon(Icons.home, color: Colors.white),
+              title: Text("Home", style: TextStyle(color: Colors.white))),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 25.0),
+            child: ListTile(
+              leading: Icon(Icons.info, color: Colors.white),
+              title: Text("About", style: TextStyle(color: Colors.white))),
+          ),
+          
+        ],
+        ),
+        const Padding(
+            padding: EdgeInsets.only(left: 25.0, bottom: 25),
+            child: ListTile(
+              leading: Icon(Icons.logout, color: Colors.white),
+              title: Text("Logout", style: TextStyle(color: Colors.white))),
+          ),
+          
+        ],),
+      ),
     );
   }
 }
